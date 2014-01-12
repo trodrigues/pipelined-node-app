e0cb209cd8b8a086389327e963dcdbf05ae372d5
Tiago Rodrigues <tmcrodrigues@gmail.com>
2014-01-12 22:01:04 +0100

    stuff

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var Mincer = require('mincer');
+
+var app = express();
+var assetEnv = new Mincer.Environment();
+assetEnv.appendPath('public/javascripts');
+assetEnv.appendPath('public/stylesheets');
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/assets', Mincer.createServer(assetEnv));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
